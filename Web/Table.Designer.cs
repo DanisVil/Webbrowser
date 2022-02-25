cbfa097ac85052e7ec3ea3977fce0ac7cebd1bb8
Danis222 <danisvil@mail.ru>
2022-02-25 23:58:27 +0300

    23:59 ееее (но не все)

--- Web/Table.Designer.cs
+++ Web/Table.Designer.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+using System.IO;
 namespace Web
 {
     partial class Table
@@ -29,20 +30,126 @@ namespace Web
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(600, 366);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer2.Size = new System.Drawing.Size(200, 366);
+            this.splitContainer2.SplitterDistance = 227;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(200, 227);
+            this.treeView1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(396, 366);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Фамилия";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Имя";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Описание";
+            this.Column3.Name = "Column3";
             // 
             // Table
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Table";
             this.Text = "Table";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProperClose);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- Web/Table.cs
+++ Web/Table.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using System;
+using System.IO;
 
 namespace Web
 {
     public partial class Table : Form
     {
+        Random random = new Random();
         private static Table instance;
         private Table()
         {
             InitializeComponent();
+            InitializeTimer();
+            InitializeTree();
         }
         public static Table GetInstance()
         {
@@ -27,5 +24,40 @@ namespace Web
         {
             instance = null;
         }
+        private void InitializeTimer()
+        {
+            // Call this procedure when the application starts.  
+            // Set to 1 second.  
+            timer1.Interval = 5000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            // Enable timer.  
+            timer1.Enabled = true;
+        }
+        private void InitializeTree()
+        {
+            string[] names1 = File.ReadAllLines($"{Environment.CurrentDirectory}\\09-121.txt");
+            string[] names2 = File.ReadAllLines($"{Environment.CurrentDirectory}\\09-122.txt");
+            System.Windows.Forms.TreeNode[] groupNodes1 = new System.Windows.Forms.TreeNode[names1.Length],
+                groupNodes2 = new System.Windows.Forms.TreeNode[names2.Length];
+            for (int i = 0; i < names1.Length; i++)
+            {
+                groupNodes1[i] = new System.Windows.Forms.TreeNode(names1[i]);
+            }
+            for (int i = 0; i < names2.Length; i++)
+            {
+                groupNodes2[i] = new System.Windows.Forms.TreeNode(names2[i]);
+            }
+            System.Windows.Forms.TreeNode group1 = new System.Windows.Forms.TreeNode("09-121", groupNodes1);
+            System.Windows.Forms.TreeNode group2 = new System.Windows.Forms.TreeNode("09-122", groupNodes2);
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            group1, group2});
+        }
+        private void timer1_Tick(object Sender, EventArgs e)
+        {
+            splitContainer2.Panel2.BackColor = System.Drawing.Color.FromArgb(random.Next(255), 
+                random.Next(255), random.Next(255));
+        }
+
     }
 }
